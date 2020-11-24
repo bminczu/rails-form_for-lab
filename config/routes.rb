fb9ffaa37985b9fec61d2c22640404c6324169ca
bminczu <bminczu@gmail.com>
2020-11-23 19:11:09 -0600

    commit tree 5a4593e96c733e7d52c5af1072050084ccfe98db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  get 'students/new'
+
+  get 'students/create'
+
+  get 'students/show'
+
+  get 'students/edit'
+
+  get 'students/update'
+
+  resources :school_classes, only: [:show, :edit, :update, :create, :new]
+  resources :students, only: [:show, :edit, :update, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
